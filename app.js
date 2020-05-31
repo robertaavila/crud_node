5b710656dd8bc26f98432d9834b43bb2bdb9b14a
robertaavila <roberta.avila@sc.senai.br>
2020-05-31 16:53:23 -0300

    Adicionando cors e express-validator.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const taskRouter = require('./routes/tasks');
 const app = express();
 
-const port = 3001;
+constport = 3001;
 
+app.use(express.json());
+app.use(cors());
 app.use(`/task`, taskRouter);
 
 app.listen(port, () => console.log('TODO app runing on port ' + port));
