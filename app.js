c6e538e56291eb73625ec52b79af13bdd1c11d8c
robertaavila <roberta.avila@sc.senai.br>
2020-06-04 20:02:48 -0300

    adicionando get e get by id

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const taskRouter = require('./routes/tasks');
-const Task = require('models/tasks.js');
+const Task = require('./models/tasks.js');
 
 const app = express();
 
-constport = 3001;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors());
 app.use(`/task`, taskRouter);
 
-app.listen(port, () => console.log('TODO app runing on port ' + port));
+app.listen(port, () => console.log('TODO app running on port ' + port));
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,14 +3,57 @@ const {check, validationResult} = require('express-validator');
 const Task = require('../models/tasks.js');
 
 router.get('/', (req, res) => {
-        res.send('<h1>Lista de tarefas</h1>');
-    }
-);
+    Task.findAll().then(tasks => {
+        if (tasks.length > 0) {
+            return res.json({
+                data: tasks
+            });
+        } else {
+            return res.status(404).json({
+                error: [{
+                    value: ' ',
+                    msg: 'Nenhuma tarefa encontrada.'
+                }]
+            });
+        }
+    });
+});
 
-router.get('/:id', (req, res) => {
-        res.send(`<h1>Detalhes da tarefa ${req.params.id}</h1>`);
+router.get('/:id', [
+    check('id', 'id deve ser um número inteiro.')
+        .trim()
+        .escape()
+        .isInt()
+        .toInt()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        Task.findByPk(req.params.id).then(task => {
+            if(task) {
+                return res.json({
+                    data: [task]
+                });
+            } else {
+                return res.status(404).json({
+                    error: [{
+                        value: '',
+                        msg: 'Tarefa não encontrada.'
+                    }]
+                })
+            }
+        }).catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                error: [{
+                    value: ' ',
+                    msg: 'Falha ao comunicar com o SGBD.'
+                }]
+            });
+        });
+    } else {
+        return res.status(422).json(errors);
     }
-);
+});
 
 router.post('/', [
     check('name', 'Nome é campo obrigatório.')
