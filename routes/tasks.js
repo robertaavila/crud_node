270f33def963265dbf68e0357e3f16bc22c0f03c
robertaavila <roberta.avila@sc.senai.br>
2020-06-04 20:51:23 -0300

    crud finalizado

--- routes/tasks.js
+++ routes/tasks.js
@@ -84,14 +84,102 @@ router.post('/', [
     }
 });
 
-router.put('/:id', (req, res) => {
-        res.send(`<h1>Editar a tarefa ${req.params.id}</h1>`);
+router.put('/:id', [
+    check('name', 'Nome é campo obrigatório.')
+        .trim()
+        .escape()
+        .notEmpty(),
+    check('id', 'id deve ser um número inteiro.')
+        .trim()
+        .escape()
+        .isInt()
+        .toInt()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        Task.update({
+            name: req.body.name,
+            description: req.body.description
+        }, {
+            where:{
+                id: req.params.id
+                }
+            }).then(rows => {
+                if (rows[0] === 1) {
+                    return res.json({
+                        data: rows[0]
+                    });
+                } else if (rows[0] === 0) {
+                    return res.status(404).json({
+                        error: [{
+                            value: ' ',
+                            msg: 'Tarefa não encontrada.'
+                        }]
+                    });
+                }
+            }).catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                error: [{
+                    value: ' ',
+                    msg: 'Falha ao comunicar com o SGBD.'
+                }]
+            });
+        });
+    } else {
+        return res.status(422).json(errors);
     }
-);
+});
 
-router.delete('/:id', (req, res) => {
-        res.send(`<h1>Deletar uma tarefa ${req.params.id}</h1>`)
+router.delete('/:id', [
+    check('id', 'id deve ser um número inteiro.')
+        .trim()
+        .escape()
+        .isInt()
+        .toInt()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        Task.destroy({
+            where:{
+                id: req.params.id
+            }
+        }).then(rows => {
+            if (rows === 1) {
+                return res.json({
+                    data: [{
+                        rows: rows
+                    }]
+                });
+            } else if (rows === 0) {
+                return res.status(404).json({
+                    error: [{
+                        value: ' ',
+                        msg: 'Tarefa não encontrada.'
+                    }]
+                });
+            } else  {
+                console.log(error);
+                return res.status(500).json({
+                    error: [{
+                        value: ' ',
+                        msg: 'Tarefa não encontrada.'
+                    }]
+                });
+            }
+        }).catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                error: [{
+                    value: ' ',
+                    msg: 'Falha ao comunicar com o SGBD.'
+                }]
+            });
+        });
+    } else {
+        return res.status(422).json(errors);
     }
-)
+});
+
 
 module.exports = router;
